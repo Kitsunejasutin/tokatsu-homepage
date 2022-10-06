c8cf13603bfb817c9bcd7545755b40e507f01451
Kitsunejasutin <justineryandeguia@yahoo.com>
2022-10-06 13:39:22 +0800

    Add Takuya Matsuyama's Blog

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,15 @@
-import { Box } from '@chakra-ui/react'
+import { 
+  Box,
+  Link } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} Takuya Matsuyama & Justine Ryan J. De Guia. All Rights Reserved.
+      &copy; {new Date().getFullYear()} {' '}
+      <Link href="https://www.craftz.dog/" target="_blank">
+        Takuya Matsuyama 
+      </Link>
+      {' '}& Justine Ryan J. De Guia. All Rights Reserved.
     </Box>
   )
 }
