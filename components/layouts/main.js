47d9bb520d7b5a64453e9931d722dba567cdfd74
Kitsunejasutin <justineryandeguia@yahoo.com>
2022-10-06 19:13:35 +0800

    Put correct values

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -8,20 +8,20 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Justine's homepage" />
-        <meta name="author" content="Takuya Matsuyama" />
-        <meta name="author" content="craftzdog" />
+        <meta name="description" content="Justine's E-Portfolio" />
+        <meta name="author" content="Justine Ryan J. De Guia" />
+        <meta name="author" content="Tomkatsu" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="twitter:title" content="Justine Ryan J. De Guia" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta name="twitter:site" content="@lolimouto01" />
+        <meta name="twitter:creator" content="@lolimouto01" />
+        <meta name="twitter:image" content="https://i.ibb.co/BZjWCyf/image.png" />
+        <meta property="og:site_name" content="Justine Ryan J. De Guia" />
+        <meta name="og:title" content="Justine Ryan J. De Guia" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        <meta property="og:image" content="https://i.ibb.co/BZjWCyf/image.png" />
         <title>Justine Ryan J. De Guia</title>
       </Head>
 
